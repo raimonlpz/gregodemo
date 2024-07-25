dd77e861f4078c8940fe161c0c46af2368489c18
Raimon López <raimon@bokokode.com>
2024-07-25 19:37:20 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { Environment, MeshReflectorMaterial } from "@react-three/drei";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
+    <Canvas shadows camera={{ position: [0, 0, 10], fov: 30 }}>
+      {/* <color attach="background" args={["#ececec"]} /> */}
+      <fog attach="fog" args={['#17171b', 30, 40]} />
+      <color attach="background" args={['#17171b']} />
+      <directionalLight castShadow intensity={1} position={[10, 6, 6]} shadow-mapSize={[1024, 1024]}>
+        <orthographicCamera attach="shadow-camera" left={-20} right={20} top={20} bottom={-20} />
+      </directionalLight>
       <Experience />
+      <mesh position={[0, -1.5, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+          <planeGeometry args={[50, 50]} />
+          <MeshReflectorMaterial
+            blur={[400, 100]}
+            resolution={1024}
+            mixBlur={1}
+            mixStrength={15}
+            depthScale={1}
+            minDepthThreshold={0.85}
+            color="#151515"
+            metalness={0.6}
+            roughness={1}
+          />
+        </mesh>
+        <Environment preset="dawn" />
     </Canvas>
   );
 }
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,96 @@
-import { OrbitControls } from "@react-three/drei";
+import { CameraControls, Environment, MeshPortalMaterial, OrbitControls, RoundedBox, Text, useTexture } from "@react-three/drei";
+import * as THREE from 'three'
+import { Nebula } from "./Nebula";
+import { Staff } from "./Staff";
+import { Orbit } from "./Orbit";
+import { useEffect, useRef, useState } from "react";
+import { useFrame, useThree } from "@react-three/fiber";
+import { easing } from "maath";
 
 export const Experience = () => {
+
+  const [active, setActive] = useState(null)
+  const controlsRef = useRef()
+  const scene = useThree(state => state.scene)
+
+  useEffect(() => {
+    if (active) {
+      const targetPosition = new THREE.Vector3()
+      scene.getObjectByName(active).getWorldPosition(targetPosition)
+      controlsRef.current.setLookAt(
+        0,
+        0,
+        5,
+        targetPosition.x,
+        targetPosition.y,
+        targetPosition.z,
+        true
+      )
+    } else {
+      controlsRef.current.setLookAt(
+        0,
+        0,
+        10,
+        0,
+        0,
+        0,
+        true
+      )
+    }
+  }, [active])
+
   return (
     <>
-      <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <ambientLight intensity={1} />
+      <Environment preset="sunset" />
+      {/* <OrbitControls /> */}
+      <CameraControls ref={controlsRef} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 6} />
+
+      <PortalStage texture={'textures/refloruit.jpg'} title={'Refloruit'} color="white" active={active} setActive={setActive}>
+        <Orbit scale={0.55} />
+      </PortalStage>
+
+      <PortalStage texture={'textures/spiritum.jpg'} position-x={-2.5} rotation-y={Math.PI / 8} title={'Spiritum'} color="white"  active={active} setActive={setActive}>
+        <Nebula scale={0.55} />
+      </PortalStage>
+
+      <PortalStage texture={'textures/aquarum.jpg'} position-x={2.5} rotation-y={-Math.PI / 8} title={'Aquarum'} color="white"  active={active} setActive={setActive}>
+        <Staff scale={0.06} />
+      </PortalStage>
+
     </>
   );
 };
+
+
+const PortalStage = ({ children, texture, title, color, active, setActive, ...props}) => {
+  const map = useTexture(texture)
+  const portalMaterial = useRef()
+
+  useFrame((_state, delta) => {
+    const worldOpen = active === title 
+    easing.damp(portalMaterial.current, 'blend', worldOpen ? 1 : 0, 0.2, delta)
+  })
+
+
+  return (
+    <group {...props}>
+      <Text font={'fonts/Cinzel.ttf'} fontSize={0.15} position={[0, -1.3, 0.051]} anchorY={'bottom'}>
+        {title}
+        <meshBasicMaterial color={color} toneMapped={false} />
+      </Text>
+      <RoundedBox name={title} args={[2,3,0.1]} onDoubleClick={() => setActive(active === title ? null : title)}>
+        {/* <planeGeometry args={[2, 3]} /> */}
+        <MeshPortalMaterial ref={portalMaterial} side={THREE.DoubleSide} > 
+          <ambientLight intensity={1} />
+          <Environment preset="sunset" />
+          {children}
+          <mesh>
+            <sphereGeometry args={[5, 64,  64]} />
+            <meshStandardMaterial map={map} side={THREE.BackSide} />
+          </mesh>
+        </MeshPortalMaterial>
+      </RoundedBox>
+    </group>
+  )
+}
