81895bd3b8cc1eed3c6a8713af4f7fc0ee9519ed
Raimon López <raimon@bokokode.com>
2024-08-20 20:37:14 +0200

    s

--- src/views/Film.jsx
+++ src/views/Film.jsx
@@ -58,7 +58,7 @@ function Film() {
             </Suspense>
             </Canvas>
             <div className="player-wrapper">
-              <ReactPlayer width={'100%'} height={'100%'}  url='https://www.youtube.com/watch?v=QNu5vlsJ4Jg' />
+              <ReactPlayer width={'100%'} height={'100%'}  url='https://www.youtube.com/embed/QNu5vlsJ4Jg?si=9_uwMqmY1Dh7QLTl' />
             </div>
             <NavLink to="/" className="button nav arrow">&#8592;</NavLink>
         </div>
