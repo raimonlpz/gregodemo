abddf3a74236f966d3f735d508a8b0a1b682eade
Raimon López <raimon@bokokode.com>
2024-08-20 20:17:46 +0200

    added Film screen

--- src/views/Agenda.jsx
+++ src/views/Agenda.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { TweenMax, TimelineMax, Power3, Power4 } from "gsap";
 import { useRef, useEffect } from "react";
 import { NavLink } from 'react-router-dom';
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
+import { Cloud } from '@react-three/drei';
 
 function Agenda() {
     let screen = useRef(null);
@@ -38,6 +41,17 @@ function Agenda() {
             <div className="load-screen1" ref={(el) => (screen = el)}></div>
         </div>
         <div ref={(el) => (body = el)} className="Agenda">
+            <Canvas className='canvas' shadows camera={{ position: [0, 20, 10], fov: 30 }}>
+              <directionalLight castShadow intensity={1} position={[10, 6, 6]} shadow-mapSize={[1024, 1024]}>
+                <orthographicCamera attach="shadow-camera" left={-20} right={20} top={20} bottom={-20} />
+              </directionalLight>
+              <Suspense fallback={null}>
+              <Cloud color={'#e0e7d3'} position={[1, 2, -15]} speed={0.2} opacity={0.25} />
+              <Cloud color={'#dfd9c9'} position={[-4, 1, -10]} speed={0.2} opacity={0.2} />
+              <Cloud color={'#e1ced9'} position={[1, 0, 0]} speed={0.2} opacity={0.15} />
+              <Cloud color={'#e1ced9'} position={[3, 0, 0]} speed={0.2} opacity={0.15} />
+            </Suspense>
+            </Canvas>
             <NavLink to="/" className="button nav arrow">&#8592;</NavLink>
         </div>
     </React.Fragment>
--- src/views/Film.jsx
+++ src/views/Film.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { TweenMax, TimelineMax, Power3, Power4 } from "gsap";
 import { useRef, useEffect } from "react";
 import { NavLink } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
+import { Cloud } from '@react-three/drei';
 
 function Film() {
     let screen = useRef(null);
@@ -40,6 +44,22 @@ function Film() {
             <div className="load-screen" ref={(el) => (screen = el)}></div>
         </div>
         <div ref={(el) => (body = el)} className="Film">
+            <Canvas className='canvas' shadows camera={{ position: [0, 20, 10], fov: 30 }}>
+              <directionalLight castShadow intensity={1} position={[10, 6, 6]} shadow-mapSize={[1024, 1024]}>
+                <orthographicCamera attach="shadow-camera" left={-20} right={20} top={20} bottom={-20} />
+              </directionalLight>
+              <Suspense fallback={null}>
+              <Cloud color={'#e6f0f3'} position={[-2, -2, -25]} speed={0.2} opacity={0.1} />
+              <Cloud color={'#e0e7d3'} position={[1, 2, -15]} speed={0.2} opacity={0.25} />
+              <Cloud color={'#dfd9c9'} position={[-4, 1, -10]} speed={0.2} opacity={0.2} />
+              <Cloud color={'#d7cee1'} position={[2, -2, -5]} speed={0.2} opacity={0.15} />
+              <Cloud color={'#e1ced9'} position={[1, 0, 0]} speed={0.2} opacity={0.15} />
+              <Cloud color={'#e1ced9'} position={[3, 0, 0]} speed={0.2} opacity={0.15} />
+            </Suspense>
+            </Canvas>
+            <div className="player-wrapper">
+              <ReactPlayer width={'100%'} height={'100%'} playing={true} url='https://www.youtube.com/watch?v=QNu5vlsJ4Jg' />
+            </div>
             <NavLink to="/" className="button nav arrow">&#8592;</NavLink>
         </div>
     </React.Fragment>
